1bed43e4dc25fb3e27ed38fbb5a9678159e23152
Pete Seeling <peteseeling@gmail.com>
2021-11-05 20:55:08 -0400

    2nd commit

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
+import { add } from 'utilities.js';
 // import functions and grab DOM elements
 const addInput1 = document.getElementById('input1');
-console.log(addInput1);
-
 const addInput2= document.getElementById('input2');
-console.log(addInput2);
-
 const button1 = document.getElementById('button1');
-console.log(button1)
-
-const result = document.getElementById('Result')
-console.log('result')
-// initialize global state
-
-// set event listeners 
-
+const result = document.getElementById('result');
+//
 button1.addEventListener('click', ()=>{
-  const input1 = +addInput1.value
-  const input2 = +addInput2.value;
-
-  const result = add(Number1, Number2);
+  const number1 = +input1.value;
+  const number2 = +input2.value;
+  const result = add(number1, number2);
+  addResult.textContent = result.value;
+  
 })
-  // get user input
+ 
   // use user input to update state 
   // update DOM to reflect the new state
